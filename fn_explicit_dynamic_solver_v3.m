d9c68cb231f4152539efe6f68b70deb78d01ae0a
paulwilcox1971 <p.wilcox@bristol.ac.uk>
2023-11-24 07:57:59 +0000

    Added more general versions of dynamic solver
    New version used global matrix indices instead of node/dof pairs and also allow displacement input as well as / instead of force input.

---
+++ fn_explicit_dynamic_solver_v3.m
@@ -0,0 +1,177 @@
+function [history_output, field_output, force_output] = fn_explicit_dynamic_solver_v3(...
+    K, M, time, ...
+    forcing_indices, forcing_functions, ...
+    disp_indices, disp_functions, ...
+    history_indices, field_output_every_n_frames, use_diagonal_lumped_mass_matrix, varargin)
+%SUMMARY
+%   Solves explicit dynamic FE problem given applied displacements or
+%   applied forces
+%INPUTS
+%   K - m x m global stiffness matrix
+%   M - m x m global mass matrix
+%   time = n-element vector of times
+%   forcing_indices - p-element vector of global matrix indices at which forcing functions
+%   will be applied
+%   forcing_functions - p x n matrix of forces to apply
+%   disp_indices - q-element vector of global matrix indices at which
+%   displacements will be imposed
+%   disp_functions - q x n matrix of displacements to apply
+%   output every field_output_every_n_frames (set to inf to get no field
+%   output)
+%   history_nodes - q-element vector of global matrix indices for complete 
+%   time-history outputs
+%   field_output_every_n_frames - complete displacement field will be
+%   use_diagonal_lumped_mass_matrix - forces use of diagonal mass matrix,
+%   which is faster, possibly less accurate
+%OUTPUTS
+%   field_output - m x floor(n / field_output_every_n_frames) matrix of displacements at all nodes
+%   history_output - q x n matrix of time histories
+
+%--------------------------------------------------------------------------
+
+if isempty(varargin)
+	use_gpu_if_present = 1;
+else
+	use_gpu_if_present = varargin{1};
+end
+
+gpu_present = fn_test_if_gpu_present_and_working;
+if use_gpu_if_present && gpu_present
+	use_gpu = 1;
+else
+	use_gpu = 0;
+end
+
+%Error checks
+if size(K,1) ~= size(K, 2)
+    error('K must be square matrix');
+end
+if size(M,1) ~= size(M, 2)
+    error('M must be square matrix');
+end
+% if size(applied_forces, 2) ~= 2 | size(applied_displacements, 2) ~= 2
+%     error('Second dimension of applied_forces and applied_displacements must be 2');
+% end
+% if size(K,1) ~= size(applied_forces,1) * size(applied_forces,1) | size(K,1) ~= size(applied_displacements,1) * size(applied_displacements,2)
+%     error('Product of first two dimensions of applied_forces and applied_displacements matrices must equal size of K');
+% end
+
+%initialise history and field output variables
+if ~history_indices
+    history_output = zeros(length(history_indices), length(time));
+end
+if ~isinf(field_output_every_n_frames)
+    field_output_ti = 1:field_output_every_n_frames:length(time);
+    field_output = zeros(size(K, 1), length(field_output_ti));
+else
+    field_output_ti = [];
+    field_output = [];
+end
+
+force_output = zeros(length(disp_indices), length(time));
+
+if use_diagonal_lumped_mass_matrix
+    tmp = sum(M);
+    M = spdiags(sum(M).', 0, size(K,1), size(K,2)); 
+    inv_M = 1 ./ M;
+end
+
+u_previous = zeros(size(K, 1), 1);
+u_dot_previous = zeros(size(K, 1), 1);
+use_gpu
+if use_gpu
+	K = gpuArray(K);
+	u_previous = gpuArray(u_previous);
+	u_dot_previous = gpuArray(u_dot_previous);
+	if use_diagonal_lumped_mass_matrix
+		inv_M = gpuArray(inv_M);
+	else
+		M = gpuArray(M);
+	end
+	if ~history_indices
+		history_indices = gpuArray(history_indices);
+		history_output = gpuArray(history_output);
+	end
+	if ~isinf(field_output_every_n_frames)
+		field_output = gpuArray(field_output);
+	end
+end
+
+accn = zeros(size(disp_functions));
+accn(:, 2:end - 1) = (disp_functions(:, 3:end) - 2 * disp_functions(:, 2:end-1) + disp_functions(:, 1:end-2)) / time_step ^ 2;
+
+
+%Main time marching loop
+time_step = time(2) - time(1);
+t1 = clock;
+for ti = 1:length(time)
+    %set force at forcing node equal to excitation signal at this instant
+    %in time
+	if use_gpu
+		f = gpuArray.zeros(size(K, 1), 1);
+	else
+		f = zeros(size(K, 1), 1);
+	end
+    if ti <= size(forcing_functions, 2)
+        f(forcing_indices) = forcing_functions(:, ti);
+    end
+    
+    %Impose displacements
+    u_previous(disp_indices) = disp_functions(:, ti);
+    
+    %work out acceleration
+    if use_diagonal_lumped_mass_matrix
+        u_dot_dot_previous = inv_M * (f - K * u_previous);
+    else
+        u_dot_dot_previous = M \ (f - K * u_previous);
+    end
+    
+    %At nodes where displacement enforced, acceleration is calculated
+    %directly
+    u_dot_dot_previous(disp_indices) = accn(:, ti);
+    %work out velocity
+    u_dot = u_dot_previous + time_step * u_dot_dot_previous;
+    %work out displacement at next time step
+    u = u_previous + time_step * u_dot;
+    
+    %history output
+    history_output(:, ti) = u(history_indices);
+
+    %force output
+    force_output(:, ti)  = 
+%     if ti == 200
+%         p_indices =[    9739,        9740];
+%         tmp = M * u_dot_dot_previous;
+%         disp(tmp(p_indices));
+%         tmp = K * u_previous;
+%         disp(tmp(p_indices));
+%         keyboard
+%     end
+    
+    %field output
+    [tmp, fi] = ismember(ti, field_output_ti);
+    if tmp
+        field_output(:, fi) = u;
+    end
+    
+    %overwrite previous values with current ones ready for next loop
+    u_previous = u;
+    u_dot_previous = u_dot;
+    
+    %Show how far through calculation is
+    fprintf('Time step %i of %i\n', ti, length(time));
+end
+if use_gpu
+	if ~history_indices
+		history_output = gather(history_output);
+	end
+	if ~isinf(field_output_every_n_frames)
+		field_output = gather(field_output);
+	end
+end
+
+t2 = etime(clock, t1);
+fprintf('    ... completed in %.2f secs\n', t2);
+
+
+end
